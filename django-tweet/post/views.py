ed093d669bf2f87bf78f7704c3fcb2b7247dd9a3
unknown <foumar404@gmail.com>
2021-10-19 13:39:44 +0000

    django tweet

---
+++ django-tweet/post/views.py
@@ -0,0 +1,22 @@
+import tweepy
+from django.conf import settings
+from django.shortcuts import render, redirect
+
+
+def index(request):
+    if request.method == 'POST':
+        content = request.POST.get('content', )
+
+        if content:
+            print('Content:', content)
+
+            auth = tweepy.OAuthHandler(
+                settings.API_KEY, settings.API_KEY_SECRET)
+            auth.set_access_token(settings.ACCESS_TOKEN,
+                                  settings.ACCES_TOKEN_SECRET)
+
+            api = tweepy.API(auth)
+            api.update_status(content)
+
+            return redirect('index')
+    return render(request, 'post/index.html')
